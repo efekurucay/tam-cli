e929a12ead7c29d7e9491e70b20632ff9219ea96
Yahya Efe Kuruçay <efekurucay@hotmail.com>
2026-03-17 05:14:54 +0300

    feat: add Homebrew Cask formula
    - Formula/alias-manager.rb: brew install --cask efekurucay/tap/alias-manager
    - Includes macOS Sonoma+ requirement
    - zap cleans ~/.config/alias-manager metadata

---
+++ Formula/alias-manager.rb
@@ -0,0 +1,17 @@
+cask "alias-manager" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/efekurucay/terminal-alias-manager/releases/download/v#{version}/AliasManager.dmg"
+  name "AliasManager"
+  desc "Visual manager for zsh terminal aliases"
+  homepage "https://github.com/efekurucay/terminal-alias-manager"
+
+  depends_on macos: ">= :sonoma"
+
+  app "AliasManager.app"
+
+  zap trash: [
+    "~/.config/alias-manager",
+  ]
+end
